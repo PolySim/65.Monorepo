42195943e154a8732eb4ad2f95f70b4e3d20353d
Simon Desdevises <simon.desdevises@gmail.com>
2025-08-18 18:19:16 +0200

    update port

--- apps/65.api/src/main.ts
+++ apps/65.api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { config as conf } from './config/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3001);
+  await app.listen(conf.port);
 }
 bootstrap();
